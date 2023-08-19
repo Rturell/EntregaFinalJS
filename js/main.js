111cdfe3a6609aa722fbfb35183f068e8106b682
rodrigoturell <rodrigoturell@gmail.com>
2023-08-19 18:14:19 -0300

    change scroll number

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 window.onscroll = function () {
     var navMobile = document.getElementById('nav-mobile');
     var scroll = window.scrollY;
-    if (scroll > 200) {
+    if (scroll > 50) {
         navMobile.style.backgroundColor = 'black';
         navMobile.style.transition = 'ease-in 0.5s';
     } else {
